02a3b3fe08fb4eb691819e3a5b8448fc6c70b23a
Laztaitai <104891477+Laztaitai@users.noreply.github.com>
2022-06-22 11:53:03 +0300

    Still not compiled

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,14 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
+		return (0);
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
