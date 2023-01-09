2845e51dbb70b762c1a2b367ed14ed5ad34e3220
Laztaitai <llateedah@gmail.com>
2023-01-09 01:20:53 -0800

    Task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int binary_search_recursive(int *array, int value, int start, int end);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+#endif
